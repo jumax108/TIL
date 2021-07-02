0f330552a36bb8d90c5ed7f5c097a681536cf800
Juhwan Choi <jumax108@kakao.com>
2021-07-02 16:39:14 +0900

    priority

---
+++ 간단한 프로그램/C++/Priority/BELOW_NORMAL_PRIORITY_CLASS/BELOW_NORMAL_PRIORITY_CLASS.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+int wmain(int argc, WCHAR* argv[]) {
+
+	SetPriorityClass(GetCurrentProcess(), BELOW_NORMAL_PRIORITY_CLASS);
+
+	for (;;) {
+
+		for (DWORD i = 0; i < 100000000; i++);
+
+		wprintf(L"BELOW_NORMAL_PRIORITY_CLASS Process\n");
+
+	}
+
+	return 0;
+
+}
---
+++ 간단한 프로그램/C++/Priority/NORMAL_PRIORITY_CLASS/NORMAL_PRIORITY_CLASS.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+int wmain(int argc, WCHAR* argv[]) {
+
+	SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
+
+	for (;;) {
+
+		for (DWORD i = 0; i < 100000000; i++) {}
+
+		wprintf(L"NORMAL_PRIORITY_CLASS\n");
+
+	}
+
+	return 0;
+}
