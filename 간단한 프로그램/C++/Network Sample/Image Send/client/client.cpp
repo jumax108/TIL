a6f9e26c73221b321a1b903cd0587af96a55a6fd
Juhwan Choi <jumax108@kakao.com>
2021-08-04 12:19:15 +0900

    네트워크 프로그래밍 연습 이미지 보내기 서버 클라

---
+++ 간단한 프로그램/C++/Network Sample/Image Send/client/client.cpp
@@ -0,0 +1,120 @@
+﻿#include <stdio.h>
+#include <locale>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32")
+
+const char* SERVER_IP = "127.0.0.1";
+constexpr int SERVER_PORT = 9000;
+constexpr int BUFFER_SIZE = 1000;
+
+struct stPacketHeader {
+
+	DWORD code;
+	WCHAR name[32];
+	WCHAR fileName[128];
+	int fileSize;
+
+};
+
+void printWSAError() {
+	int error = WSAGetLastError();
+	wprintf(L"wsa error = %d\n", error);
+}
+void printError() {
+	int error = GetLastError();
+	wprintf(L"error = %d\n", error);
+}
+
+
+int main() {
+
+	SOCKET sock = NULL;
+
+	setlocale(LC_ALL, "ko-KR");
+	do {
+
+		FILE* imgFile;
+		_wfopen_s(&imgFile, L"냥이.jpg", L"rb");
+		if (imgFile == nullptr) {
+			printError();
+			break;
+		}
+		int fileSize;
+		fseek(imgFile, 0, SEEK_END);
+		fileSize = ftell(imgFile);
+		rewind(imgFile);
+
+		BYTE* fileBuffer = (BYTE*)malloc(sizeof(BYTE) * fileSize);
+		fread_s(fileBuffer, sizeof(BYTE) * fileSize, sizeof(BYTE), fileSize, imgFile);
+
+		WSADATA wsa;
+		if (WSAStartup(MAKEWORD(2, 2), &wsa) != NULL) {
+			printWSAError();
+			break;
+		}
+
+		sock = socket(AF_INET, SOCK_STREAM, 0);
+		if (sock == INVALID_SOCKET) {
+			printWSAError();
+			break;
+		}
+
+		SOCKADDR_IN serverAddr;
+		ZeroMemory(&serverAddr, sizeof(SOCKADDR_IN));
+		serverAddr.sin_family = AF_INET;
+		InetPtonA(AF_INET, SERVER_IP, &serverAddr.sin_addr.s_addr);
+		serverAddr.sin_port = htons(SERVER_PORT);
+		int result = connect(sock, (SOCKADDR*)&serverAddr, sizeof(SOCKADDR_IN));
+		if (result == SOCKET_ERROR) {
+			printWSAError();
+			break;
+		}
+
+		stPacketHeader packetHeader;
+		packetHeader.code = 0x11223344;
+		packetHeader.fileSize = fileSize;
+		wcscpy_s(packetHeader.name, 32, L"최주환");
+		wcscpy_s(packetHeader.fileName, 128, L"저희집냥이에요.jpg");
+		result = send(sock, (const char*)&packetHeader, sizeof(stPacketHeader), 0);
+		if (result == SOCKET_ERROR) {
+			printWSAError();
+			break;
+		}
+
+		while (fileSize > 0) {
+
+			int sendLen = 1000;
+			if (sendLen > fileSize) {
+				sendLen = fileSize;
+			} 
+
+			result = send(sock, (const char*)fileBuffer, sendLen, 0);
+			if (result == SOCKET_ERROR){
+				printWSAError();
+				break;
+			}
+			fileBuffer += sendLen;
+			fileSize -= sendLen;
+
+			wprintf(L"%d Byte 보냄, %d Byte 남음\n", sendLen, fileSize);
+
+		}
+
+		char buf[255];
+		result = recv(sock, buf, 255, 0);
+		if (result == SOCKET_ERROR) {
+			printWSAError();
+			break;
+		}
+		wprintf(L"recv %x%x%x%x\n", buf[0], buf[1], buf[2], buf[3]);
+		
+	} while (false);
+
+
+	closesocket(sock);
+	WSACleanup();
+
+	system("PAUSE>NUL");
+	return 0;
+}
---
+++ 간단한 프로그램/C++/Network Sample/Image Send/server/server.cpp
@@ -0,0 +1,179 @@
+﻿#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <locale.h>
+
+#include "ringBuffer.h"
+
+#pragma comment(lib, "ws2_32")
+
+
+struct stPacketHeader {
+
+	DWORD code;
+	WCHAR name[32];
+	WCHAR fileName[128];
+	int fileSize;
+
+};
+
+constexpr int SERVER_PORT = 9000;
+constexpr int BUFFER_SIZE = 1460;
+constexpr int RINGBUFFER_SIZE = BUFFER_SIZE * 2;
+
+void printWSAError() {
+	int error = WSAGetLastError();
+	wprintf(L"wsa error = %d\n", error);
+}
+
+void printError() {
+	int error = GetLastError();
+	wprintf(L"error = %d\n", error);
+}
+
+int main() {
+	setlocale(LC_ALL, "ko-KR");
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != NULL) {
+		printWSAError();
+		return 1;
+	}
+
+	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET) {
+		printWSAError();
+		return 1;
+	}
+
+	SOCKADDR_IN serverAddr;
+	ZeroMemory(&serverAddr, sizeof(SOCKADDR_IN));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(SERVER_PORT);
+	int result = bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+	if (result == SOCKET_ERROR) {
+		printWSAError();
+		return 1;
+	}
+	
+	result = listen(listenSocket, SOMAXCONN);
+	if (result == SOCKET_ERROR) {
+		printWSAError();
+		return 1;
+	}
+
+	SOCKET clientSocket;
+	SOCKADDR_IN clientAddr;
+	int addrLen;
+
+	CRingBuffer ringBuffer(RINGBUFFER_SIZE);
+	char buf[BUFFER_SIZE + 1];
+
+	stPacketHeader packetHeader;
+
+	bool findHeader = false;
+
+	BYTE* fileData = nullptr;
+	BYTE* fileLastData = nullptr;
+	int fileDataLen;
+
+	while (1) {
+
+		addrLen = sizeof(clientAddr);
+		clientSocket = accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET) {
+			printWSAError();
+			break;
+		}
+		InetNtopA(AF_INET, &clientAddr.sin_addr, buf, BUFFER_SIZE);
+		printf("클라이언트 접속: IP = %s, PORT = %d\n", buf, ntohs(clientAddr.sin_port));
+
+		stPacketHeader packetHeader;
+		ZeroMemory(&packetHeader, sizeof(stPacketHeader));
+		while (1) {
+
+			ZeroMemory(buf, BUFFER_SIZE + 1);
+			result = recv(clientSocket, buf, BUFFER_SIZE, 0);
+			if (result == SOCKET_ERROR || result == 0) {
+				printWSAError();
+				break;
+			}
+
+			ringBuffer.push(result, (const BYTE*)buf);
+
+			if (findHeader == false){
+				if (ringBuffer.size() >= sizeof(stPacketHeader)) {
+
+					ringBuffer.front(sizeof(stPacketHeader), (BYTE*)&packetHeader);
+					ringBuffer.pop(sizeof(stPacketHeader));
+					findHeader = true;
+					wprintf(L"[VALUE] %x %s\n", packetHeader.code, packetHeader.name);
+				}
+			}
+			else {
+
+				if (fileData == nullptr) {
+					fileData = (BYTE*)malloc(sizeof(BYTE) * packetHeader.fileSize);
+					fileLastData = fileData;
+					fileDataLen = 0;
+				}
+
+				int popDataLen = ringBuffer.size();
+				if (popDataLen + fileDataLen > packetHeader.fileSize) {
+					popDataLen = packetHeader.fileSize - fileDataLen;
+				}
+
+				ringBuffer.front(popDataLen, fileLastData);
+				fileLastData += popDataLen;
+				fileDataLen += popDataLen;
+				ringBuffer.pop(popDataLen);
+				wprintf(L"%d Byte 받음, %d Byte 남음, 전체 %d Byte\n", popDataLen, packetHeader.fileSize - fileDataLen, packetHeader.fileSize);
+
+				if (fileDataLen == packetHeader.fileSize) {
+
+					WCHAR* fileName = (WCHAR*)malloc(sizeof(WCHAR) * (wcslen(packetHeader.fileName) + wcslen(packetHeader.name) + 2));
+					wsprintf(fileName, L"%s_%s", packetHeader.name, packetHeader.fileName);
+
+					FILE* writeFile;
+					_wfopen_s(&writeFile, fileName, L"wb");
+					if (writeFile == nullptr) {
+						printError();
+						break;
+					}
+
+					fwrite(fileData, sizeof(BYTE), fileDataLen, writeFile);
+					fclose(writeFile);
+
+					wprintf(L"파일저장 !\n");
+
+					free(fileData);
+					fileData = nullptr;
+					findHeader = false;
+
+					buf[0] = 0xdd;
+					buf[1] = 0xdd;
+					buf[2] = 0xdd;
+					buf[3] = 0xdd;
+					buf[4] = '\0';
+					send(clientSocket, buf, 4, 0);
+				}
+
+			}
+
+		}
+
+		closesocket(clientSocket);
+		InetNtopA(AF_INET, &clientAddr.sin_addr, buf, BUFFER_SIZE);
+		printf("클라이언트 종료: IP = %s, PORT = %d\n", buf, ntohs(clientAddr.sin_port));
+
+		findHeader = false;
+		free(fileData);
+		fileData = nullptr;
+
+	}
+	
+	closesocket(listenSocket);
+	WSACleanup();
+
+	return 0;
+}
