293859543790f749acf5c434366e67c8c2478ffe
Juhwan Choi <jumax108@kakao.com>
2021-08-31 11:58:15 +0900

    직렬화 버퍼

---
+++ 자료구조/protocolBuffer/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <memory.h>
+#include <Windows.h>
+#include <malloc.h>
+#include <time.h>
+#include "protocolBuffer.h"
+int main() {
+
+	CProtocolBuffer pb(20);
+
+
+
+	return 0;
+
+}
